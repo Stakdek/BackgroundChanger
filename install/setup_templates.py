c96f97c8fab0125e18bfdbdcca496ed122cc7f80
Dennis Wulfert <dennis.wulfert@perfact.de>
2019-04-02 09:51:54 +0200

    new python installer

--- install/setup_templates.py
+++ install/setup_templates.py
@@ -6,41 +6,24 @@ import os
 import getpass
 from os.path import expanduser
 
-if len(sys.argv) > 2:
-    picpath = str(sys.argv[2]).strip()
-    if picpath.endswith('/'):
-        picpath = picpath + '*'
-    else:
-        picpath = picpath + '/*'
-else:
-    picpath = '$HOME/*'
+def run(interval,picpath):
+    replace_map = {
+        '#HOME#' : expanduser("~"),
+        '#IVAN-PATH#' : os.path.dirname(os.path.realpath(__file__)).replace('/install',''),
+        '#USER#' : getpass.getuser(),
+        '#PICPATH#' : picpath,
+        '#INTERVAL#': str(interval),
+    }
+    output=[]
 
-if len(sys.argv) > 1:
-    interval = str(sys.argv[1]).strip()
-    try:
-        interval = int(interval)
-    except ValueError:
-        interval = 300
-else:
-    interval = 300
+    with open('install/backgroundchanger_template','r') as service_temp_file:
+        service_temp = service_temp_file.readlines()
+        for line in service_temp:
+            for key in replace_map.keys():
+                if key in line:
+                    line = line.replace(key, replace_map[key])
+            output.append(line)
 
-
-replace_map = {
-    '#HOME#' : expanduser("~"),
-    '#IVAN-PATH#' : os.path.dirname(os.path.realpath(__file__)).replace('/install',''),
-    '#USER#' : getpass.getuser(),
-    '#PICPATH#' : picpath,
-    '#INTERVAL#': str(interval),
-}
-output=[]
-
-with open('install/backgroundchanger_template','r') as service_temp_file:
-    service_temp = service_temp_file.readlines()
-    for line in service_temp:
-        for key in replace_map.keys():
-            if key in line:
-                line = line.replace(key, replace_map[key])
-        output.append(line)
-
-with open('backgroundchanger', 'w') as service_file:
-    service_file.writelines(output)
+    with open('backgroundchanger', 'w') as service_file:
+        service_file.writelines(output)
+    return
